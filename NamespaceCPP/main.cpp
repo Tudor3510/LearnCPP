b2f06f68305ea1cd0bbc2ffd20b7fa81ef8c3e5c
Tudor3510 <tudorcioc@gmail.com>
2023-08-11 17:15:05 +0300

    Working with namespaces and ADL

---
+++ NamespaceCPP/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// THIS WORKS !!!!!!!
+/*
+namespace Whoa
+{
+    class Test
+    {
+    };
+
+    void DoStuff(Test t)
+    {
+        std::cout << "Doing stuff in namespace Whoa\n";
+    }
+}
+
+int main()
+{
+    Whoa::Test test;
+
+    DoStuff(test);              // ADL
+    // ADL stands for "Argument-Dependent Lookup," which is a mechanism
+    // in C++ that allows unqualified function or operator names to be
+    // looked up in the namespaces of the function arguments
+
+
+    return 0;
+}
+*/
