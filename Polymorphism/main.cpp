e10a733950ed0ca8231ea845db3ec76c97e2bcd1
Tudor3510 <tudorcioc@gmail.com>
2023-08-12 19:15:05 +0300

    More on polymorphism with const and override

--- Polymorphism/main.cpp
+++ Polymorphism/main.cpp
@@ -159,3 +159,382 @@ int main(){
     return 0;
 }
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS WORKS !!!!!!!!!
+/*
+class BaseClass
+{
+    public:
+        virtual void PrintText() const
+        {
+            cout << "Hello from BaseClass" << "\n";
+        }
+};
+
+
+class DerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const
+        {
+            cout << "Hello from DerivedClass" << "\n";
+        }
+};
+
+
+class ThirdDerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const
+        {
+            cout << "Hello from ThirdDerivedClass" << "\n";
+        }
+};
+
+int main ()
+{
+    BaseClass *baseClassPtr;
+    
+    baseClassPtr = new DerivedClass();
+    baseClassPtr->PrintText();                          // Hello from DerivedClass
+
+    cout << "\n";
+
+    baseClassPtr = new ThirdDerivedClass();
+    baseClassPtr->PrintText();                          // Hello from ThirdDerivedClass
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS WORKS !!!!!!!!!!!!!
+// but be careful how it works
+/*
+class BaseClass
+{
+    public:
+        virtual void PrintText()
+        {
+            cout << "Hello from BaseClass" << "\n";
+        }
+};
+
+
+class DerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const
+        {
+            cout << "Hello from DerivedClass" << "\n";
+        }
+};
+
+
+class ThirdDerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const
+        {
+            cout << "Hello from ThirdDerivedClass" << "\n";
+        }
+};
+
+int main ()
+{
+    BaseClass *baseClassPtr;
+    
+    baseClassPtr = new DerivedClass();
+    baseClassPtr->PrintText();                          // Hello from BaseClass
+
+    cout << "\n";
+
+    baseClassPtr = new ThirdDerivedClass();
+    baseClassPtr->PrintText();                          // Hello from BaseClass
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS DOES NOT WORK !!!!!!
+// void DerivedClass::PrintText() const' marked 'override', but does not override
+/*
+class BaseClass
+{
+    public:
+        virtual void PrintText()
+        {
+            cout << "Hello from BaseClass" << "\n";
+        }
+};
+
+
+class DerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const override
+        {
+            cout << "Hello from DerivedClass" << "\n";
+        }
+};
+
+
+class ThirdDerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const override
+        {
+            cout << "Hello from ThirdDerivedClass" << "\n";
+        }
+};
+
+int main ()
+{
+    BaseClass *baseClassPtr;
+    
+    baseClassPtr = new DerivedClass();
+    baseClassPtr->PrintText();
+
+    cout << "\n";
+
+    baseClassPtr = new ThirdDerivedClass();
+    baseClassPtr->PrintText();
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS DOES NOT WORK !!!!!!!!
+// member function declared with 'override' does not override a base class member
+// so you should put all the overridden function definition from the baseclass before the 'const' qualifier
+/*
+class BaseClass
+{
+    public:
+        virtual void PrintText() const
+        {
+            cout << "Hello from BaseClass" << "\n";
+        }
+};
+
+
+class DerivedClass : public BaseClass
+{
+    public:
+        void PrintText() override const
+        {
+            cout << "Hello from DerivedClass" << "\n";
+        }
+};
+
+
+class ThirdDerivedClass : public BaseClass
+{
+    public:
+        void PrintText() override const
+        {
+            cout << "Hello from ThirdDerivedClass" << "\n";
+        }
+};
+
+int main ()
+{
+    BaseClass *baseClassPtr;
+    
+    baseClassPtr = new DerivedClass();
+    baseClassPtr->PrintText();
+
+    cout << "\n";
+
+    baseClassPtr = new ThirdDerivedClass();
+    baseClassPtr->PrintText();
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS WORKS !!!!!!!!
+/*
+class BaseClass
+{
+    public:
+        virtual void PrintText() const
+        {
+            cout << "Hello from BaseClass" << "\n";
+        }
+};
+
+
+class DerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const override
+        {
+            cout << "Hello from DerivedClass" << "\n";
+        }
+};
+
+
+class ThirdDerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const override
+        {
+            cout << "Hello from ThirdDerivedClass" << "\n";
+        }
+};
+
+int main ()
+{
+    BaseClass *baseClassPtr;
+    
+    baseClassPtr = new DerivedClass();
+    baseClassPtr->PrintText();                          // Hello from DerivedClass
+
+    cout << "\n";
+
+    baseClassPtr = new ThirdDerivedClass();
+    baseClassPtr->PrintText();                          // Hello from ThirdDerivedClass
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS WORKS !!!!!!!!!!
+/*
+class BaseClass
+{
+    public:
+        virtual void PrintText() const
+        {
+            cout << "Hello from BaseClass" << "\n";
+        }
+};
+
+
+class DerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const override
+        {
+            BaseClass::PrintText();
+            cout << "Hello from DerivedClass" << "\n";
+        }
+};
+
+
+class ThirdDerivedClass : public BaseClass
+{
+    public:
+        void PrintText() const override
+        {
+            cout << "Hello from ThirdDerivedClass" << "\n";
+        }
+};
+
+int main ()
+{
+    BaseClass *baseClassPtr;
+    
+    baseClassPtr = new DerivedClass();
+    baseClassPtr->PrintText();                          // Hello from BaseClass
+                                                        // Hello from DerivedClass
+
+    cout << "\n";
+
+    baseClassPtr = new ThirdDerivedClass();
+    baseClassPtr->PrintText();                          // Hello from ThirdDerivedClass
+
+    return 0;
+}
+*/
