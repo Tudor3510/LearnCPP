0662bea68d2d14f380194e7ae6dddf6131aab1bf
Tudor3510 <tudorcioc@gmail.com>
2023-08-15 13:31:51 +0300

    Interface for Weapon

---
+++ JasonBusbyCode/C2-C3/Fireball.h
@@ -0,0 +1,8 @@
+#ifndef FIREBALL_H
+#define FIREBALL_H
+
+#include "IWeapon.h"
+
+
+
+#endif
---
+++ JasonBusbyCode/C2-C3/IWeapon.h
@@ -0,0 +1,15 @@
+#ifndef IWEAPON_H
+#define IWEAPON_H
+
+#include "IEntity.h"
+#include "Vector2.h"
+
+class IWeapon
+{
+public:
+    virtual void Attach(IEntity& entity) = 0;
+    virtual void Fire(IEntity& entity, const Vector2& position) = 0;
+    virtual bool IsActive(IEntity& entity) = 0;
+};
+
+#endif
