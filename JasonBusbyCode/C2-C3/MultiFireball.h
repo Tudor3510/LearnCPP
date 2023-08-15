c65b0daf279104c12421374444db23c3dc01d547
Tudor3510 <tudorcioc@gmail.com>
2023-08-15 15:45:09 +0300

    MultiFireball header file for the class

---
+++ JasonBusbyCode/C2-C3/MultiFireball.h
@@ -0,0 +1,25 @@
+#ifndef MULTIFIREBALL_H
+#define MULTIFIREBALL_H
+
+#include <map>
+#include "IWeapon.h"
+#include "Level.h"
+#include "IEntity.h"
+
+class MultiFireball : public IWeapon
+{
+    std::map<IEntity*, int> _ammo;
+    char _character;
+    Level& _level;
+    int _damage;
+
+public:
+    MultiFireball(char character, Level& level, int damage);
+
+    void Attach(IEntity& entity);
+    void Fire(IEntity& entity, const Vector2& direction);
+    bool IsActive(IEntity& entity);
+};
+
+
+#endif
