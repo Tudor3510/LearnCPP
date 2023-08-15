18716ab5351ce4077b693dc65eafa2f7733507b2
Tudor3510 <tudorcioc@gmail.com>
2023-08-15 13:34:43 +0300

    Header for Fireball weapon

--- JasonBusbyCode/C2-C3/Fireball.h
+++ JasonBusbyCode/C2-C3/Fireball.h
@@ -3,6 +3,12 @@
 
 #include "IWeapon.h"
 
-
+class Fireball : public IWeapon
+{
+public:
+    void Attach(IEntity& entity);
+    void Fire(IEntity& entity, const Vector2& position);
+    bool IsActive(IEntity& entity);
+};
 
 #endif
