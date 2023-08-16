48ab5a5d496e9fdf53ecb5f3580e01e82bab99d0
Tudor3510 <tudorcioc@gmail.com>
2023-08-16 23:54:58 +0300

    Initialization list with the members of the parent class

--- InitializationList/main.cpp
+++ InitializationList/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
-//So you can use the same name
+// THIS WOKRS !!!!!!
+// So you can use the same name
+/*
 class ClassA
 {
     int x, y;
@@ -17,8 +18,43 @@ public:
 
 int main(){
     ClassA* classA = new ClassA(10, 12);
-    cout    <<  classA->GetX()  <<  "\n"    <<  classA->GetY()    <<  "\n";
-
+    cout << classA->GetX() << "\n"          // 10
+         << classA->GetY() << "\n";         // 12
 
     return 0;
 }
+*/
+
+
+
+
+
+
+// THIS DOES NOT WORK !!!!!!
+// class 'DerivedClass' does not have any field named '_text'
+// You can not initialize members of the parent class in the derived class constructor initialization list.
+// It doesn't matter whether they are protected, public or anything else.
+/*
+class BaseClass
+{
+protected:
+    std::string _text;
+};
+
+class DerivedClass : public BaseClass
+{
+public:
+    DerivedClass(std::string text) : _text(text)
+    {
+    }
+
+};
+
+
+int main()
+{
+    DerivedClass derivedClass("Un text");
+
+    return 0;
+}
+*/
